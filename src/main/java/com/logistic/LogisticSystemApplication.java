e1c6809f06d615e16a6621922aa8b656dc25d637
nicolasholanda <nikolas.nhm14@gmail.com>
2026-03-26 10:02:57 -0300

    Initial commit

---
+++ src/main/java/com/logistic/LogisticSystemApplication.java
@@ -0,0 +1,12 @@
+package com.logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticSystemApplication.class, args);
+    }
+}
